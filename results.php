98862a1a951dbdb402fc0da3d1c645ddc3942061
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-15 02:10:15 +0000

    Fixed comments results.php

--- results.php
+++ results.php
@@ -3,6 +3,7 @@
   define("MAX", 6);
   $correctNumber = intval(rand(MIN, MAX));
   $guessedNumber = intval($_POST["user-guess"]);
+
   //if/then statement: if correctNumber is equal to guessedNumber
   if ($correctNumber == $guessedNumber) {
     echo "Good job, that is correct!";
