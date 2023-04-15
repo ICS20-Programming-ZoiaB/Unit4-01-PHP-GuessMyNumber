37903193b23a68fc5ddea646a37ed675e799f508
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-15 02:22:11 +0000

    Took out closing PHP

--- results.php
+++ results.php
@@ -4,13 +4,12 @@
   $correctNumber = intval(rand(MIN, MAX));
   $guessedNumber = intval($_POST["user-guess"]);
 
-  //if/then statement: if correctNumber is equal to guessedNumber
+//if/then statement: if correctNumber is equal to guessedNumber
   if ($correctNumber == $guessedNumber) {
     echo "Good job, that is correct!";
   }
-  
-  //if/then statement: if correctNumber is not equal to guessedNumber
+
+//if/then statement: if correctNumber is not equal to guessedNumber
  if ($correctNumber != $guessedNumber) {
-    echo "You guessed the wrong number! The correct number is $correctNumber.";
+   echo "You guessed the wrong number! The correct number is $correctNumber.";
   }
-?>
